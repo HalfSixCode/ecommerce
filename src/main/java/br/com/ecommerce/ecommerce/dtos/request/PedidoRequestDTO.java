5c21b6c8bfe669c5a613ecbc8ae6278fb05e0a64
Davziinn <mdavi6085@gmail.com>
2025-06-26 22:30:37 -0300

    pedido-DTOs feito

---
+++ src/main/java/br/com/ecommerce/ecommerce/dtos/request/PedidoRequestDTO.java
@@ -0,0 +1,15 @@
+package br.com.ecommerce.ecommerce.dtos.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
+import java.util.UUID;
+
+public record PedidoRequestDTO (
+
+        @NotBlank
+        UUID userId
+
+        //@NotBlank
+        //List<ItemPedidoRequestDTO> items
+) {}
